45a67e86e699a70caa3231015327528a6c6da28a
JoeStaff <bthompson89@ymail.com>
2023-05-06 04:58:22 +0000

    debug option and game name for rg35xx build script

--- packaging/rg35xx/build-garlicos.sh
+++ packaging/rg35xx/build-garlicos.sh
@@ -9,6 +9,7 @@ fi
 set -e
 
 # Set default values
+GAME_PROPER_NAME="Example Game"
 BUILD_DIR="build-rg35xx-garlic"
 WORKSPACE=$(dirname $(dirname $(pwd)))
 ZIP_NAME=$(basename "$WORKSPACE")"-rg35xx-garlic.zip"
@@ -29,11 +30,16 @@ while [[ $# -gt 0 ]]; do
         CLEAN="true"
         shift # past argument
         ;;
+        --debug)
+        DEBUG="true"
+        shift # past argument
+        ;;
         *)    # unknown option
         echo "Unknown option: $1"
         echo "Options:"
         echo "-j # | run make with designated number of cores, default: 1"
         echo "--clean | wipes build directory before running"
+        echo "--debug | enables debug logging and options"
         exit 1
         ;;
     esac
@@ -62,7 +68,10 @@ fi
 
 # Run cmake with CXXFLAGS appended to the command line
 cd "$WORKSPACE/$BUILD_DIR" || exit
-cmake .. -DCMAKE_CXX_FLAGS="$CXXFLAGS"
+if [ "$DEBUG" == "true" ]; then
+    DEBUG_APPEND="-DDEBUG=ON"
+fi
+cmake .. -DCMAKE_CXX_FLAGS="$CXXFLAGS" $DEBUG_APPEND
 
 # Run make with the specified number of cores (if any)
 if [ ! -z "$CORES" ]; then
@@ -73,14 +82,14 @@ fi
 
 # Prepare package environment
 cp -R $PACKAGESPACE/package .
+mkdir package/ROMS/PORTS/$(basename "$WORKSPACE")
 cd bin && cp * ../package/ROMS/PORTS/$(basename "$WORKSPACE") && cd ..
-cd .. && cp -R assets $BUILD_DIR/package/ROMS/PORTS/ && cd $BUILD_DIR
-
+cd .. && cp -R assets $BUILD_DIR/package/ROMS/PORTS/$(basename "$WORKSPACE") && cd $BUILD_DIR
 cd package
 # Modify launch script and icon
 sed -i "s/GAME_NAME/$(basename "$WORKSPACE")/g" ./ROMS/PORTS/GAME_NAME.sh
-mv ROMS/PORTS/GAME_NAME.sh ROMS/PORTS/$(basename "$WORKSPACE").png
-mv ROMS/PORTS/Imgs/GAME_NAME.png ROMS/PORTS/Imgs/$(basename "$WORKSPACE").png
+mv ROMS/PORTS/GAME_NAME.sh "ROMS/PORTS/$GAME_PROPER_NAME.sh"
+mv ROMS/PORTS/Imgs/GAME_NAME.png "ROMS/PORTS/Imgs/$GAME_PROPER_NAME.png"
 
 # Generate zip
 zip -r ../$ZIP_NAME *
